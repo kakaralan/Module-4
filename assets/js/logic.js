c3956f1b656b254e78ba397aec0f0fdd2e9ac136
neel k <neelk@neels-MacBook-Pro.local>
2022-09-14 17:23:08 -0500

    NK2

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,9 @@
-// variables to keep track of quiz state
+
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
-// variables to reference DOM elements
+
 var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
@@ -12,51 +12,38 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
-
 function startQuiz() {
-  // hide start screen
   var startScreenEl = document.getElementById("start-screen");
   startScreenEl.setAttribute("class", "hide");
 
-  // un-hide questions section
   questionsEl.removeAttribute("class");
 
-  // start timer
   timerId = setInterval(clockTick, 1000);
 
-  // show starting time
   timerEl.textContent = time;
 
   getQuestion();
 }
 
 function getQuestion() {
-  // get current question object from array
   var currentQuestion = questions[currentQuestionIndex];
 
-  // update title with current question
   var titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.title;
 
-  // clear out any old question choices
   choicesEl.innerHTML = "";
 
-  // loop over choices
   currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
+
     var choiceNode = document.createElement("button");
     choiceNode.setAttribute("class", "choice");
     choiceNode.setAttribute("value", choice);
 
     choiceNode.textContent = i + 1 + ". " + choice;
 
-    // attach click event listener to each choice
     choiceNode.onclick = questionClick;
 
-    // display on the page
+
     choicesEl.appendChild(choiceNode);
   });
 }
